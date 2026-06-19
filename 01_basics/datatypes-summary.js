6f31c6dac388eeee7c7521027f98156c58521212
Phulpati-git <phulpati0264@xavier.edu.np>
2026-06-19 08:53:24 +0000

    basics

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// # Primitive
+
+// 7 types : String, Number , Boolean , null , undefined , symbol , BigInt
+
+
+const score = 100 
+const scoreValue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 2297384782784723n
+// reference (Non-primitive)
+
+// Array , obects , Functions 
+
+const heros = ['ironman','power','panther'];
+let myObject = {
+    name: "Phulpati",
+    age:22,
+}
+
+const myFunction = function() {
+    console.log("Hello ,world");
+}
+
+console.log(typeof anotherId);
