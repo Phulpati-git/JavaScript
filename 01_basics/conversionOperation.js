5e5a4fba86bba4813ca9b022b1fd253dda739113
Phulpati-git <phulpati0264@xavier.edu.np>
2026-06-19 02:01:33 +0000

    basics

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -20,8 +20,38 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // "" => false
 // "Phulpati" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof (stringNumber));
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof (stringNumber));
+
+/*******************opeartions********************** */
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2%3); mofulous is important in algorithms and stuff 
+// let str1 = "meow"
+// let str2 = " cat"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1+'2');
+// console.log('1'+2);
+// console.log(1+2+"3");
+// console.log(1+'2'+ 3);     ? messy conversions
+
+// console.log(true);
+// console.log(+true); dont do these kinda of parenthesis
+// console.log(+"");
+
+// let number1,number2,number3;
+// number1=number2=number3 = 2          dont do this as code readibility is necessary 
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+
+
 
