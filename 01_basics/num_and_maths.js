24a2b8efa0c8295037727abda16779a58b4e938f
Phulpati-git <phulpati0264@xavier.edu.np>
2026-06-21 06:38:43 +0000

    basics

---
+++ 01_basics/num_and_maths.js
@@ -0,0 +1,35 @@
+// const score = 4000
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 23.9234
+// console.log(otherNumber.toPrecision(4));
+
+// const hundreddds = 100000
+// // console.log(hundreddds.toLocaleString('en-NP'));
+
+
+
+// // ++++++++++++++++ maathssssss++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(2,5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2 , 4 , 5 ));
+// console.log(Math.max(2 ,3 ,4 ,5));
+console.log((Math.random()*10 )+1 );
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10 
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+min)
+
