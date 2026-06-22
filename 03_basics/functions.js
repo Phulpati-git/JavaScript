cff6abf2c928f490c7d597319e60d1efe3dce330
Phulpati-git <phulpati0264@xavier.edu.np>
2026-06-22 09:00:00 +0000

    basics_3

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -34,5 +34,34 @@ function loginUserMessage(username = "Meow"){
 }
 
 // console.log(loginUserMessage("phulpati"))
-console.log(loginUserMessage("XD"))
+// console.log(loginUserMessage("XD"))
 
+function calculateCartPrice(val1, val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400 , 500, 2000))
+
+const user = {
+    username : "Phulpati",
+    price : "10000000"
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+    
+}
+// handleObject(user)
+handleObject({
+    username : "sam",
+    price : "200"
+})
+
+const myNewArray = [ 200 ,400 ,100 ,300 ]
+
+function returnSecondValue(getArray){
+    return getArray[3]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200 ,300 , 3000 , 4000]));
