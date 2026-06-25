36f6d8716aa2b37fd0d281227d032cea9f7f8e82
Phulpati-git <phulpati0264@xavier.edu.np>
2026-06-25 07:26:28 +0000

    basics_4

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,59 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3
+
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("febuary");
+//         break;
+//     case 3:
+//         console.log("march");
+//         break;
+//     case 4:
+//         console.log("april");
+//         break;
+//     case 5:
+//         console.log("may");
+//         break;
+
+//     default:
+//         console.log("default");
+        
+//         break;
+// }
+
+const month = "April"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("febuary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "April":
+        console.log("april");
+        break;
+    case "may":
+        console.log("may");
+        break;
+
+    default:
+        console.log("default");
+        
+        break;
+}
