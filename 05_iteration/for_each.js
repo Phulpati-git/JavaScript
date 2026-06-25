a3b8bd02f8cd259eab3c980ceed0566e7abf4e56
Phulpati-git <phulpati0264@xavier.edu.np>
2026-06-25 10:20:39 +0000

    basics_5

---
+++ 05_iteration/for_each.js
@@ -0,0 +1,40 @@
+// const coding = ["js", "runy", "java" , "cpp"]
+
+// // coding.forEach(function (item) {
+// //     console.log(item);
+// // })
+
+// // coding.forEach(  (greet) => {
+// //     console.log(greet)
+// // } )
+
+// function printMe(item){
+//     console.log(item);
+    
+// }
+// coding.array.forEach(printMe)
+
+// coding.array.forEach((item , index , arr) =>{
+//     console.log(item, index , arr);
+    
+// });
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFIleName: "js"
+    },
+    {
+        languageName: "java",
+        languageFIleName: "java"
+    },
+    {
+        languageName: "python",
+        languageFIleName: "py"
+    },
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+    
+});
