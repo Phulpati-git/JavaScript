778c65ebf1688a8dd1c500fadb57043ad7598f74
Phulpati-git <phulpati0264@xavier.edu.np>
2026-06-25 10:07:12 +0000

    basics_5

---
+++ 05_iteration/for_of2.js
@@ -0,0 +1,19 @@
+// const myObj = {
+//     js: 'javascript',
+//     cpp : 'C++',
+//     rb: 'ruby',
+//     swift: 'swift '
+// }
+
+// for (const key in myObj) {
+//     // console.log(`${key} shortcut is for ${myObj[key]}`);
+    
+// }
+
+// const programming = [ "js","py", "java","cpp"]
+
+// for (const key in programming) {   
+//     // console.log(programming[key]);
+     
+// }
+
