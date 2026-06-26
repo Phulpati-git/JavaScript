9121d918b1bd86b9157a426fca42cad0676df13b
Phulpati-git <phulpati0264@xavier.edu.np>
2026-06-26 07:22:21 +0000

    basics_5

---
+++ 05_iteration/for_each4.js
@@ -0,0 +1,14 @@
+const myNumbers = [1 , 2, 3, 4, 5]
+
+// const newNumns = myNumbers.map( (num) =>{
+//     return num + 10
+// })
+
+
+const newNums = myNumbers
+       .map((num) => num * 10)
+       .map((num) => num + 1)
+       .filter( (num) =>num >= 40)
+console.log(newNums);
+
+
